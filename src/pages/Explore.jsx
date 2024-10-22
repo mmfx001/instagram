e08a51aed108f32f0ee49b8f56d2e771fb4d6f0c
behhruz <bsatimbayev05@gmail.com>
2024-10-22 19:33:40 +0500

    done

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Explore = () => {
   return (
-    <div  className="flex justify-center items-center h-full text-2xl font-bold" >Explore</div>
+    <div className="flex justify-center items-center h-full text-2xl font-bold" >Explore</div>
   )
 }
 
 export default Explore
+
