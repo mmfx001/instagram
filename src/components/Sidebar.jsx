0826bb9c5eb0e01ea6d22d148273257e573c2293
komronbekn <nurdullaevkomron0@gmail.com>
2024-10-24 19:32:45 +0500

    by komron

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,37 +18,37 @@ const Sidebar = () => {
         Instagram
       </Link>
       <nav className="flex flex-col space-y-6 flex-grow">
-        <Link to="/home" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500">
+        <Link to="/home" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500 focus:text-blue-500">
           <AiFillHome size={24} />
           <span>Home</span>
         </Link>
-        <Link to="/search" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500">
+        <Link to="/search" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500 focus:text-blue-500">
           <AiOutlineSearch size={24} />
           <span>Search</span>
         </Link>
-        <Link to="/explore" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500">
+        <Link to="/explore" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500 focus:text-blue-500">
           <AiOutlineCompass size={24} />
           <span>Explore</span>
         </Link>
-        <Link to="/messages" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500">
+        <Link to="/messages" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500 focus:text-blue-500">
           <AiOutlineMessage size={24} />
           <span>Messages</span>
         </Link>
-        <Link to="/notifications" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500">
+        <Link to="/notifications" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500 focus:text-blue-500">
           <AiOutlineHeart size={24} />
           <span>Notifications</span>
         </Link>
-        <Link to="/create" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500">
+        <Link to="/create" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500 focus:text-blue-500">
           <AiOutlinePlusSquare size={24} />
           <span>Create</span>
         </Link>
-        <Link to="/profile" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500">
+        <Link to="/profile" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500 focus:text-blue-500">
           <AiOutlineUser size={24} />
           <span>Profile</span>
         </Link>
       </nav>
       <div className="mt-auto">
-        <Link to="/more" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500">
+        <Link to="/more" className="flex items-center space-x-4 text-lg text-black hover:text-blue-500 focus:text-blue-500">
           <AiOutlineEllipsis size={24} />
           <span>More</span>
         </Link>
